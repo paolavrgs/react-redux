74123ab6c9684f378b1527406e5170f862dceec6
Paola Vargas <vrgspaola68@gmail.com>
2018-12-01 16:42:43 -0400

    Lifecycle Methods section: Constructor deleted, and state assignment out of it. ComponentDidMount data fetching

--- season-app/src/index.js
+++ season-app/src/index.js
@@ -1,35 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
+// import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props); //Calls constructor() parent
-    
-    // ONLY TIME we do direct assignment to this.state
-    this.state = {
-      lat: null, // initialized 'null' because we don't know the value yet
-      errorMessage: ''
-    }
+  state = {
+    lat: null,
+    errorMessage: ''
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        // updating the state with the setState() function
-        this.setState({
-          lat: position.coords.latitude
-        })
-        // NEVER DO THIS:
-        // this.state.lat = position.coords.latitude
-      }, err => {
-        this.setState({
-          errorMessage: err.message
-        })
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     )
   }
-  // React says we have to define render!!
+
   render() {
-    // return <div>Latitude: { this.state.lat }</div>
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>
     }
