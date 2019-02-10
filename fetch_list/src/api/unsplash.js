b3d8ad523f23ff86d8bfe51f6d2f58647b6ed65f
Paola Vargas <vrgspaola68@gmail.com>
2019-02-10 13:11:01 -0400

    Create new component ImageCard - css grid - dinamic height

--- fetch_list/src/api/unsplash.js
+++ fetch_list/src/api/unsplash.js
@@ -4,5 +4,8 @@ export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
     Authorization: 'Client-ID 682fdf00d2866c94bb93c32fd201c6ac3598cfca565b15ac6ec5c2cad0d429e5'
+  },
+  params: {
+    per_page: 30
   }
 })
---
+++ fetch_list/src/components/ImageCard.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class ImageCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.imageRef = React.createRef();
+    this.state = {
+      spans : 0
+    }
+  }
+
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans)
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans })
+  }
+
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div className="image-card" style={{ gridRowEnd: `span ${this.state.spans}`}}>
+        <img ref={this.imageRef} alt={description} src={urls.regular}/>
+      </div>
+    )
+  }
+}
